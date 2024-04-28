81a2f964ed964f839d4fda5346d9696db4bc5883
Swastik Pandey <swastikpandey@Swastiks-MacBook-Air.local>
2024-04-28 13:03:55 +0530

    comments

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-// const subHeading = React.createElement(
-//   "h1",
-//   { id: "main_heading", tabIndex: "0" },
-//   "Padh le bhai, ye laundiyabaazi me kuch bhi nhi rakha hai 💦"
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(subHeading);
-
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "Hi Welcome to the messy nested world!"),
