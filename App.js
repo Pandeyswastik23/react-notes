c1391206f5ac373c3659dc35fd43533b8eb24160
Swastik Pandey <swastikpandey@Swastiks-MacBook-Air.local>
2024-05-29 14:55:37 +0530

    component composition

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hi Welcome to the messy nested world!"),
-    React.createElement("h2", {}, "Hi welcome to subHeading"),
-  ]),
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hi Welcome to the messy nested world!"),
-    React.createElement("h2", {}, "Hi welcome to subHeading"),
-  ]),
-]);
+// React Element
+const heading = (
+  <h1 className="head" tabIndex="1">
+    Hi Welcome to the messy nested world!
+  </h1>
+);
+
+const Title = () => <h1>This is title</h1>;
+
+// React functional component
+// Component Composition
+const num = 10000;
+const HeadingComponent = () => (
+  <div id="container">
+    {num}
+    <Title />
+    <h2 className="heading1">Hello buddy</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
